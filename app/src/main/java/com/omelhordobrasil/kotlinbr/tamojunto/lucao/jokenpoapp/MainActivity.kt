8fea203e934e4c788e635ced23f771a03a8a21f9
lucas.mmarques.ext <lucas.mmarques.ext@panonline.com.br>
2023-04-12 22:25:07 -0300

    Fim do modulo 11

--- app/src/main/java/com/omelhordobrasil/kotlinbr/tamojunto/lucao/jokenpoapp/MainActivity.kt
+++ app/src/main/java/com/omelhordobrasil/kotlinbr/tamojunto/lucao/jokenpoapp/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.omelhordobrasil.kotlinbr.tamojunto.lucao.jokenpoapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
+import com.omelhordobrasil.kotlinbr.tamojunto.lucao.jokenpoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val toolbar = binding.toolbar
+
+        setContentView(binding.root)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.title  = "novo titulo pela activity"
+        supportActionBar?.setLogo(R.drawable.ic_logo)
+        supportActionBar?.setDisplayUseLogoEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val activity2Intent = Intent(this, MainActivity2::class.java)
+        intent.putExtra("name","tomaz")
+        intent.putExtra("name",38)
+
+        binding.startActivityButton.setOnClickListener {
+            startActivity(activity2Intent)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
